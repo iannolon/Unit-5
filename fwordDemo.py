73cb57165f9d4729c28f24e769427fb01ec5f5fc
iannolon <35769152+iannolon@users.noreply.github.com>
2018-04-23 08:22:49 -0400

    Update fwordDemo.py

--- fwordDemo.py
+++ fwordDemo.py
@@ -1,1 +1,10 @@
+#IanNolon
+#4/23/18
+#fwordDemo.py - print out only the words that have an f
+
+words = input('Type in some words: ').split(' ') #every time it sees a space, it will split it into different words.
+
+for item in words:
+    if 'f' in item or 'F' in item:
+        print (item) #this for loop prints out the whole loop
 
