a6c81d785e9a6684ff4145115cde24bab2cd9a18
iannolon <35769152+iannolon@users.noreply.github.com>
2018-04-25 08:28:23 -0400

    Create randomMonth.py

---
+++ randomMonth.py
@@ -0,0 +1,11 @@
+#IanNolon  
+#4/25/18
+#randomMonth.py
+
+from random import randint
+
+year = ['January','February','March','April','May','June','July','August','September','October','November','December']
+#for item in year:
+    #print(item)
+
+print(year[randint(0,11)])
