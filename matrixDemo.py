dd833c353779dc9a36126718dc1d03c638a2a191
iannolon <35769152+iannolon@users.noreply.github.com>
2018-04-27 08:33:55 -0400

    Create matrixDemo.py

---
+++ matrixDemo.py
@@ -0,0 +1,18 @@
+#IanNolon
+#4/27/18
+#matrixDemo.py - lists inside of lists
+
+def printBoard(board):
+    for r in range(0,3):
+        for c in range(0,3):
+            print(board[r][c],end=' ')
+        print()
+
+board = [['a','b','c'],['d','e','f'],['g','h','i']]
+printBoard(board)
+
+row = int(input('Enter a row: '))
+col = int(input('Enter a column: '))
+board[row-1][col-1] = 'X'
+
+printBoard(board)
