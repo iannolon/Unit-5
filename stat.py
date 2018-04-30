21c7ce376edb297e87f4a99ba40006e2b94e0c6f
iannolon <35769152+iannolon@users.noreply.github.com>
2018-04-30 08:48:47 -0400

    Update stat.py

--- stat.py
+++ stat.py
@@ -5,10 +5,23 @@
 print('Type a list of numbers')
 print('Enter \'q\' when finished')
 a = 0
+total = 0
 nums = []
 while a == 0:
     num = input('>')
-    nums.append(num)
     if num == 'q':
         a = 1
+    else:
+        total += int(num)
+        nums.append(int(num))
 print('Min:',min(nums))
+print('Mean:',total/len(nums))
+print('Max:',max(nums))
+l = 0
+for item in nums:
+    n = list.count(item)
+    if n > l:
+        l += n
+        m = (item)
+#print('Mode:',list.count('
+print([nums])
