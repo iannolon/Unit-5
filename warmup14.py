2b3841850a0069265ff776a10cc5027eca158955
iannolon <35769152+iannolon@users.noreply.github.com>
2018-04-30 08:25:12 -0400

    Create warmup14.py

---
+++ warmup14.py
@@ -0,0 +1,6 @@
+#IanNolon
+#4/29/18
+#warmup14.py - ask user for first and last name, tell them what their initials are
+
+name = input('Enter your first and last name').split(' ')
+print('Your initials are',name[0][0],'.',name[1][0],'.')
