3378a8c15f3bba20c625faf8e413ae06a66c6e3a
iannolon <35769152+iannolon@users.noreply.github.com>
2018-05-07 07:54:35 -0400

    Create listCommands.py

---
+++ listCommands.py
@@ -0,0 +1,23 @@
+#IanNolon
+#5/7/18
+#listcommands
+
+'''
+create list : coins = ['pennies','nickels']
+numbers = range(0,100,18)
+list indeces start at 0 not 1
+coins [1] is nickels
+item associated with stop index is not included
+coins.append()
+coins.remove()
+coins[0] = cent  --> changing an item
+coins.sort()
+coins.reverse()
+coins.count('quarter')
+len(coins)
+sum(numbers)
+min(numbers)
+max(numbers)
+for item in coins:
+    print item
+nested lists --> coins[0][2]
